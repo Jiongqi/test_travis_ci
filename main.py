b835d73eeb816b4665e566a898347fecdda17023
Jiongqi <qujiongqi@163.com>
2021-01-14 15:50:18 +0800

    add main.py for testing

---
+++ main.py
@@ -0,0 +1,20 @@
+import unittest
+
+class Test_Math(unittest.TestCase):
+
+    def setupclass(self):
+        print("start")
+
+    def teardownclass(self):
+        print("finish")
+
+
+    def test_addTwoNum_01(self):
+        self.assertEqual(1, 3-2)
+
+    def test_subTwoNum_02(self):
+        self.assertEqual(2, 3-1)
+
+
+unittest.main()
+
